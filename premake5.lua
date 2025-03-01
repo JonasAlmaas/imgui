05d308a91250430fc7e013457f2b964dd2358177
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-03-01 15:43:06 +0100

    Update build output

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,11 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    cppdialect "C++20"
-    staticruntime "on"
+	cppdialect "C++20"
+	staticruntime "on"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
