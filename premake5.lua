7dc6d84ae580859393a6a341a6376ec6924a09a5
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-03-10 19:44:35 +0100

    Remove staticruntime "on"

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,6 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
-	staticruntime "on"
 
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
